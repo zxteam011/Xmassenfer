d36e740afe0fab504c8bb4807c31119d3f310a14
IT'S ULL4SH <mahbub.ullash01@gmail.com>
2025-05-11 17:06:39 +0600

    Update info0.js

--- Script/commands/info0.js
+++ Script/commands/info0.js
@@ -1,74 +1,199 @@
 module.exports.config = {
-	name: "info",
-	version: "1.0.1", 
-	hasPermssion: 0,
-	credits: "Ialamick Chat", //don't change the credits please
-	description: "Admin and Bot info.",
-	commandCategory: "...",
-	cooldowns: 1,
-	dependencies: 
-	{
-    "request":"",
-    "fs-extra":"",
-    "axios":""
-  }
+ name: "info",
+ version: "1.2.6",
+ hasPermssion: 0,
+ credits: "Shaon Ahmed",
+ description: "info bot owner",
+ commandCategory: "For users",
+ hide:true,
+ usages: "",
+ cooldowns: 5,
 };
-module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
-const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【hh:mm:ss】");
-var link = ["https://i.imgur.com/WXQIgMz.jpeg", 
-            
-            "https://i.postimg.cc/QdgH08j6/Messenger-creation-C2-A39-DCF-A8-E7-4-FC7-8715-2559476-FEEF4.gif", 
-            
-            "https://i.imgur.com/ybM9Wtr.jpeg",
-            
-            "https://i.imgur.com/k8nes6h.jpeg"];
-  
-var callback = () => api.sendMessage({body:`•—»✨𝐀𝐝𝐦𝐢𝐧 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺
-•┄┅═════❁🌺❁═════┅┄•
 
- 𝐁𝐨𝐭 𝐍𝐚𝐦𝐞 : 𝐈𝐬𝐥𝐚𝐦𝐢𝐜𝐤 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
 
-𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 : 𝐂𝐘𝐁𝐄𝐑 𝐁𝐎𝐓 𝐂𝐎𝐌𝐌𝐔𝐍𝐈𝐓𝐘
+module.exports.run = async function ({ api, event, args, Users, permssion, getText ,Threads}) {
+ const content = args.slice(1, args.length);
+ const { threadID, messageID, mentions } = event;
+ const { configPath } = global.client;
+ const { ADMINBOT } = global.config;
+ const { NDH } = global.config;
+ const { userName } = global.data;
+ const request = global.nodemodule["request"];
+ const fs = global.nodemodule["fs-extra"];
+ const { writeFileSync } = global.nodemodule["fs-extra"];
+ const mention = Object.keys(mentions);
+ delete require.cache[require.resolve(configPath)];
+ var config = require(configPath);
+ const listAdmin = ADMINBOT || config.ADMINBOT || [];
+ const listNDH = NDH || config.NDH || [];
+ {
+ const PREFIX = config.PREFIX;
+ const namebot = config.BOTNAME;
+ const { commands } = global.client;
+ const threadSetting = (await Threads.getData(String(event.threadID))).data || 
+ {};
+ const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX 
+ : global.config.PREFIX;
+ const dateNow = Date.now();
+ const time = process.uptime(),
+ hours = Math.floor(time / (60 * 60)),
+ minutes = Math.floor((time % (60 * 60)) / 60),
+ seconds = Math.floor(time % 60);
+ const data = [
+ "Bạn không thể tìm được lệnh admin tại 'help' của MintBot",
+ "Đừng mong chờ gì từ MintBot.",
+ "Cái đoạn này á? Của SpermBot.",
+ "Nếu muốn không lỗi lệnh thì hãy xài những lệnh có trong help vì những lệnh lỗi đã bị ẩn rồi.",
+ "Đây là một con bot được các coder của MiraiProject nhúng tay vào.",
+ "Muốn biết sinh nhật của Mint thì hãy xài 'birthday'.",
+ "Cặc.",
+ "Cút.",
+ "Lồn.",
+ "Bạn chưa biết.",
+ "Bạn đã biết.",
+ "Bạn sẽ biết.",
+ "Không có gì là hoàn hảo, MintBot là ví dụ.",
+ "Mirai dropped.",
+ "MintBot là MiraiProject nhưng module là idea của SpermBot.",
+ "Bạn không biết cách sử dụng MintBot? Đừng dùng nữa.",
+ "Muốn chơi game? Qua bot khác mà chơi đây không rảnh",
+ "MintBot có thể hiểu phụ nữ nhưng không thể có được họ.",
+ "MintBot cân spam nhưng không có gì đáng để bạn spam."
+ ];
+ var link = [
+ "https://i.postimg.cc/QdgH08j6/Messenger-creation-C2-A39-DCF-A8-E7-4-FC7-8715-2559476-FEEF4.gif",
+ "https://i.imgur.com/WXQIgMz.jpeg",
+ "https://i.postimg.cc/QdgH08j6/Messenger-creation-C2-A39-DCF-A8-E7-4-FC7-8715-2559476-FEEF4.gif",
+ "https://i.imgur.com/WXQIgMz.jpeg",
+ "https://i.imgur.com/WXQIgMz.jpeg",
+ ];
 
-𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 : Ullash ッ
+ var i = 1;
+ var msg = [];
+ const moment = require("moment-timezone");
+ const date = moment.tz("Asia/Dhaka").format("hh:mm:ss");
+ for (const idAdmin of listAdmin) {
+ if (parseInt(idAdmin)) {
+ const name = await Users.getNameUser(idAdmin);
+ msg.push(`${i++}/ ${name} - ${idAdmin}`);
+ }
+ }
+ var msg1 = [];
+ for (const idNDH of listNDH) {
+ if (parseInt(idNDH)) {
+ const name1 = (await Users.getData(idNDH)).name
+ msg1.push(`${i++}/ ${name1} - ${idNDH}`);
+ }
+ }
+ var callback = () => 
+ api.sendMessage({ body: `====「 ${namebot} 」====\n» Prefix system: ${PREFIX}\n» Prefix box: ${prefix}\n» Modules: ${commands.size}\n» Ping: ${Date.now() - dateNow}ms\n──────────────\n======「 ADMIN 」 ======\n${msg.join("\n")}\n──────────────\nBot has been working for ${hours} hour(s) ${minutes} minute(s) ${seconds} second(s)\n\n» Total users: ${global.data.allUserID.length} \n» Total threads: ${global.data.allThreadID.length}\n──────────────\n[thanks for using bot!!]`, attachment: fs.createReadStream(__dirname + "/cache/kensu.jpg"), }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/kensu.jpg"));
+ return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/kensu.jpg")).on("close", () => callback()); 
+ }
+}/**
+ * @author Shaon Ahmed
+ * @warn Do not edit code or edit credits
+ */
 
-•┄┅══❁CONCATET❁══┅┄• 
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐈𝐝: https://www.facebook.com/9w3bzcf1cr
+module.exports.config = {
+ name: "info",
+ version: "1.2.6",
+ hasPermssion: 0,
+ credits: "Shaon Ahmed",
+ description: "🥰আসসালামু আলাইকুম 🥰",
+ commandCategory: "For users",
+ hide:true,
+ usages: "",
+ cooldowns: 5,
+};
 
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐈𝐝: https://www.facebook.com/profile.php?id=100086680386976
 
-𝐆𝐢𝐭𝐇𝐮𝐛  : https://github.com/cyber-ullash
+module.exports.run = async function ({ api, event, args, Users, permssion, getText ,Threads}) {
+ const content = args.slice(1, args.length);
+ const { threadID, messageID, mentions } = event;
+ const { configPath } = global.client;
+ const { ADMINBOT } = global.config;
+ const { NDH } = global.config;
+ const { userName } = global.data;
+ const request = global.nodemodule["request"];
+ const fs = global.nodemodule["fs-extra"];
+ const { writeFileSync } = global.nodemodule["fs-extra"];
+ const mention = Object.keys(mentions);
+ delete require.cache[require.resolve(configPath)];
+ var config = require(configPath);
+ const listAdmin = ADMINBOT || config.ADMINBOT || [];
+ const listNDH = NDH || config.NDH || [];
+ {
+ const PREFIX = config.PREFIX;
+ const namebot = config.BOTNAME;
+ const { commands } = global.client;
+ const threadSetting = (await Threads.getData(String(event.threadID))).data || 
+ {};
+ const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX 
+ : global.config.PREFIX;
+ const dateNow = Date.now();
+ const time = process.uptime(),
+ hours = Math.floor(time / (60 * 60)),
+ minutes = Math.floor((time % (60 * 60)) / 60),
+ seconds = Math.floor(time % 60);
+ const data = [
+ "Bạn không thể tìm được lệnh admin tại 'help' của MintBot",
+ "Đừng mong chờ gì từ MintBot.",
+ "Cái đoạn này á? Của SpermBot.",
+ "Nếu muốn không lỗi lệnh thì hãy xài những lệnh có trong help vì những lệnh lỗi đã bị ẩn rồi.",
+ "Đây là một con bot được các coder của MiraiProject nhúng tay vào.",
+ "Muốn biết sinh nhật của Mint thì hãy xài 'birthday'.",
+ "Cặc.",
+ "Cút.",
+ "Lồn.",
+ "Bạn chưa biết.",
+ "Bạn đã biết.",
+ "Bạn sẽ biết.",
+ "Không có gì là hoàn hảo, MintBot là ví dụ.",
+ "Mirai dropped.",
+ "MintBot là MiraiProject nhưng module là idea của SpermBot.",
+ "Bạn không biết cách sử dụng MintBot? Đừng dùng nữa.",
+ "Muốn chơi game? Qua bot khác mà chơi đây không rảnh",
+ "MintBot có thể hiểu phụ nữ nhưng không thể có được họ.",
+ "MintBot cân spam nhưng không có gì đáng để bạn spam."
+ ];
+ var link = [
+ "https://i.postimg.cc/QdgH08j6/Messenger-creation-C2-A39-DCF-A8-E7-4-FC7-8715-2559476-FEEF4.gif",
+ "https://i.imgur.com/WXQIgMz.jpeg",
+ "https://i.postimg.cc/QdgH08j6/Messenger-creation-C2-A39-DCF-A8-E7-4-FC7-8715-2559476-FEEF4.gif",
+ "https://i.imgur.com/WXQIgMz.jpeg",
+ "https://i.imgur.com/WXQIgMz.jpeg",
 
-𝐖𝐏 𝐍𝐮𝐦𝐛𝐞𝐫 : 01859*****58
+ ]; 
+ var i = 1;
+ var msg = [];
+ const moment = require("moment-timezone");
+ const date = moment.tz("Asia/Dhaka").format("hh:mm:ss");
+ for (const idAdmin of listAdmin) {
+ if (parseInt(idAdmin)) {
+ const name = await Users.getNameUser(idAdmin);
+ msg.push(`${i++}/ ${name} - ${idAdmin}`);
+ }
+ }
+ var msg1 = [];
+ for (const idNDH of listNDH) {
+ if (parseInt(idNDH)) {
+ const name1 = (await Users.getData(idNDH)).name
+ msg1.push(`${i++}/ ${name1} - ${idNDH}`);
+ }
+ }
+ var callback = () => 
+ api.sendMessage({ body: 
+ `🍀----আসসালামু আলাইকুম----🍀
 
-𝐒𝐞𝐚𝐫𝐜𝐡 𝐆𝐨𝐨𝐠𝐥𝐞 : এখনো এত বড় সেলিব্রিটি হই নাই 🤡, যে কেউ গুগলে সার্চ করবে 💔🌚
+┏━━•❅•••❈•••❈•••❅•━━┓\n\n「 ${namebot} 」\n\n┗━━•❅•••❈•••❈•••❅•━━┛ \n\n______________________________\n\n↓↓_𝗥𝗢𝗕𝗢𝗧 𝗦𝗬𝗦𝗧𝗘𝗠 𝗜𝗡𝗙𝗢_↓↓\n\n» 𝗣𝗿𝗲𝗳𝗶𝘅 𝘀𝘆𝘀𝘁𝗲𝗺: ${PREFIX}\n\n» 𝗣𝗿𝗲𝗳𝗶𝘅 𝗯𝗼𝘅: ${prefix}\n\n» 𝗧𝗼𝘁𝗮𝗹 𝗠𝗼𝗱𝘂𝗹𝗲𝘀: ${commands.size}\n\n» 𝗣𝗶𝗻𝗴: ${Date.now() - dateNow}ms\n______________________________\n\n ↓↓_𝗥𝗢𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢_↓↓\n\n 
+𝗡𝗔𝗠𝗘 :>ULLASH
 
-𝐖𝐚𝐛 𝐒𝐢𝐭𝐞 𝐋𝐢𝐧𝐤 : https://bio.link/cyber_ullash
+𝗢𝘄𝗻𝗲𝗿 𝗜𝗱 𝗹𝗶𝗻𝗸:☞https://www.facebook.com/profile.php?id=100086680386976
 
-•┄┅═════❁🌺❁═════┅┄•\n🌺✨𝐎𝐭𝐡𝐞𝐫𝐬 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺\n •┄┅═════❁🌺❁═════┅┄•
+𝗪𝗵𝗮𝘁𝘀𝗮𝗽𝗽☞01631813286
 
-TYPE /admin 
-
-𝐁𝐨𝐭 𝐍𝐚𝐦𝐞 : ${global.config.BOTNAME}
-
-𝐁𝐨𝐭 𝐏𝐫𝐞𝐟𝐢𝐱 : ${global.config.PREFIX} \\নিচের ইনফরমেশন গুলো পরিবর্তন করবেন না 
-
-𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 : Ullash ッ\n\n 𝐁𝐨𝐭 Fork link:- https://github.com/cyber-ullash/CYBER-BOT-COMMUNITY.git \n\n 𝐘𝐨𝐮𝐓𝐮𝐛𝐞 :- https://youtube.com/@cyberbotcommunity?si=vDIyqsB882bcVOt3
-
-•—»✨ 𝐔𝐩𝐭𝐢𝐦𝐞
-
-𝐓𝐨𝐝𝐚𝐲 𝐈𝐬 𝐓𝐢𝐦𝐞 : ${juswa} 
-
-𝐁𝐨𝐭 𝐈𝐬 𝐑𝐮𝐧𝐧𝐢𝐧𝐠 ${hours}:${minutes}:${seconds}.
-
-𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐢𝐧𝐠  ༄🌺\n｢🕋｣${global.config.BOTNAME}｢🕋｣`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
-   };
+______________________________\n\n----↓↓𝙍𝙤𝙗𝙤𝙩 𝙖𝙘𝙩𝙞𝙫𝙚 𝙩𝙞𝙢𝙚↓↓----\n\n ${hours} : ${minutes} : ${seconds} second(s)\n\n______________________________\n» 𝗧𝗢𝗧𝗔𝗟 𝗨𝗦𝗘𝗥𝗦: ${global.data.allUserID.length} \n\n» 𝗧𝗢𝗧𝗔𝗟 𝗚𝗥𝗢𝗨𝗣: ${global.data.allThreadID.length}\n______________________________\n\n thanks for using \n 𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 
+\n--------------------------------------------------\n\n☢️☣️⚠️`, attachment: fs.createReadStream(__dirname + "/cache/kensu.jpg"), }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/kensu.jpg"));
+ return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/kensu.jpg")).on("close", () => callback()); 
+ }
+}
