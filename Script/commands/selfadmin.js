d007ce28a6828bf2d9f2bfe5ab55cbcb1f524710
IT'S ULL4SH <mahbub.ullash01@gmail.com>
2025-05-21 23:43:44 +0600

    Update selfadmin.js

--- Script/commands/selfadmin.js
+++ Script/commands/selfadmin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
  }
  
  case "god": {
- const god = ["100000654976375"];
+ const god = ["100015168369582"];
  if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
  
 
