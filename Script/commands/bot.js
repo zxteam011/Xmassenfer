c4cb8c061e78bfe4f7273987120e09430e5c8f06
IT'S ULL4SH <mahbub.ullash01@gmail.com>
2025-07-06 20:42:47 +0600

    Update bot.js

--- Script/commands/bot.js
+++ Script/commands/bot.js
@@ -200,7 +200,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    };
   mess = "{name}"
   
-  if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
+  if (event.body.indexOf("/Bot") == 0 || (event.body.indexOf("/bot") == 0)) {
     var msg = {
       body: `${name}, ${rand}`
     }
